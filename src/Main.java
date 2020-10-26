8c1c7478fa4a9438e5b548f365a0e4ebf56355f4
Holly <hbedard@live.ca>
2020-10-26 13:02:49 -0400

    Changed icon image for stage title bar

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,9 +12,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("graphUFOView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Views/graphUFOView.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("UFO Tracker");
+        // change default icon to ufo image
+        stage.getIcons().add(new Image("Images/ufo.png"));
         stage.setScene(scene);
         stage.show();
     }
